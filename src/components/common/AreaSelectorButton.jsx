f29003f4db633f381fd81838f3b453c533b8d33c
juanPabloSarobe <jp.sarobe@gmail.com>
2025-08-10 15:43:40 -0300

    area selector button terminado

--- src/components/common/AreaSelectorButton.jsx
+++ src/components/common/AreaSelectorButton.jsx
@@ -422,7 +422,7 @@ const AreaSelectorButton = ({ markersData, onUnitSelect, map }) => {
       borderRadius: "24px",
       boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
       backgroundColor: "white",
-      display: "flex",
+      display: { xs: "none", sm: "flex" }, // Ocultar en móvil, mostrar en desktop
       alignItems: "center",
       zIndex: 1000,
       overflow: "hidden",
